ce217774d79753fb73a0909cd99220623a0a244f
yellowchihuahua <47878958+yellowchihuahua@users.noreply.github.com>
2023-10-25 18:11:56 -0400

    refactored insertmiddle and deletemiddle functions for unnecessary calls

--- Lab4/Lab4.c
+++ Lab4/Lab4.c
@@ -174,7 +174,8 @@ int IntToStr(int x, char str[], int d)
 } 
 
 //ref https://www.geeksforgeeks.org/convert-floating-point-number-string/#
-void ftoa(float n, char* res, int afterpoint) 
+//returns 0 if unsuccessful, 1 if successful
+int ftoa(float n, char* res, int afterpoint) 
 { 
     int ipart = (int)n; 
     float fpart = n - (float)ipart; 
@@ -188,30 +189,35 @@ void ftoa(float n, char* res, int afterpoint)
  
         IntToStr((int)fpart, res + i + 1, afterpoint); 
     } 
+	return 1;
 } 
 
 //ref https://stackoverflow.com/questions/25798977/returning-string-from-c-function
 //function to convert node to strictly comma separated data
 //param info extra: string is string to use; allocate WITH SIZE before calling this function. similar to scanf. refer to reference link
-void ToCsv(char *string, struct Node* node) {
+//returns 0 if unsuccessful, 1 if successful
+int ToCsv(struct Node* node, char *string) {
 	if(node == NULL){
 		printf("ToCsv(char *string, struct Node* node); -- Node is null, nothing to convert.\n");
-		return;
+		return 0;
 	}
 	
 	snprintf(string, 256, "%d, %s, %s, %s, %.2f\n", node->key, node->firstName, node->lastName, node->major, node->GPA);
+	return 1;
 }
 
 //ref https://stackoverflow.com/questions/25798977/returning-string-from-c-function
 //function to create readable format
 //param info extra: string is string to use; allocate WITH SIZE before calling this function. similar to scanf. refer to reference link
-void ToString(char *string, struct Node* node) {
+//returns 0 if unsuccessful, 1 if successful
+int ToString(struct Node* node, char *string) {
 	//todo: look into snprintf
 	if(node == NULL){
 		printf("ToString(char *string, struct Node* node); -- Node is null, nothing to convert.\n");
-		return;
+		return 0;
 	}
 	snprintf(string, 256, "Key: %d | Data: %s, %s, %s, %.2f\n", node->key, node->firstName, node->lastName, node->major, node->GPA);
+	return 1;
 }
 
 //in print format
@@ -222,7 +228,7 @@ void PrintNode(struct Node* node){
 	}
 	//printf("Key: %d | Data: %s, %s, %s, %.2f\n", node->key, node->firstName, node->lastName, node->major, node->GPA);
 	char nodeStr[256];
-	ToString(nodeStr, node);
+	ToString(node, nodeStr);
 	printf("%s", nodeStr);
 }
 
@@ -252,22 +258,34 @@ void FreeList(struct Node** start){
 
 //InsertFront
 //ref ll.c example, TA in person assistance
-void InsertFront(struct Node** start, struct Node* nodeToInsert){
+int InsertFront(struct Node** start, struct Node* nodeToInsert){
+	if(nodeToInsert == NULL) {
+		printf("InsertFront(struct Node** start, int index, struct Node* nodeToInsert); -- Node is null, nothing to insert.\n");
+		return 0;
+	}
+
 	if (GetListLength(start) == 0){
 		*start = nodeToInsert;
-		return;
+		return 1;
 	}
 	
 	nodeToInsert->next = *start; //points next var to front of list, global variable
 	*start = nodeToInsert; //inserts node and sets pointer of new front, global varaible
+	return 1;
 }
 
 //InsertEnd generalized
-void InsertEnd(struct Node** start, struct Node* nodeToInsert){
+//returns 0 if unsuccessful, 1 if successful
+int InsertEnd(struct Node** start, struct Node* nodeToInsert){
+	if(nodeToInsert == NULL) {
+		printf("InsertEnd(struct Node** start, int index, struct Node* nodeToInsert); -- Node is null, nothing to insert.\n");
+		return 0;
+	}
+
 	//check if already has items
 	if (GetListLength(start) == 0){
 		*start = nodeToInsert;
-		return;
+		return 1;
 	}
 
 	//get to the last node in linked list
@@ -276,78 +294,99 @@ void InsertEnd(struct Node** start, struct Node* nodeToInsert){
 		current = current->next;
 	}
 	current->next = nodeToInsert;
+	return 1;
 }
 
 //param index is the index before node to insert
-void InsertMiddle(struct Node** start, int index, struct Node* nodeToInsert){
+//returns 0 if unsuccessful, 1 if successful
+int InsertMiddle(struct Node** start, int index, struct Node* nodeToInsert){
+	if(nodeToInsert == NULL) {
+		printf("InsertMiddle(struct Node** start, int index, struct Node* nodeToInsert); -- Node is null, nothing to insert.\n");
+		return 0;
+	}
+
+	//check end cases
+	if(index == 0){ //if index is at beginning
+		InsertFront(start, nodeToInsert);
+		return 1;
+	}
+	if (index == GetListLength(start)) { //if index is at end
+		InsertEnd(start, nodeToInsert);
+		return 1;
+	}
+	
 	struct Node* previousNode = LookUpByIndex(start, index);
 	struct Node* nextNode = LookUpByIndex(start, index+1);
-
-	if(previousNode == NULL){ //if index is at beginning
-		InsertFront(start, nodeToInsert);
-		return;
-	}
-	if (nextNode == NULL) { //if index is at end
-		InsertEnd(start, nodeToInsert);
-		return;
-	}
 	previousNode->next = nodeToInsert;
 	nodeToInsert->next = nextNode; //linking 
+	return 1;
 }
 
+//function to streamline InsertByGPA. converts entire node to a string in order of comparison to use strcmp to sort entire thing alphabetically
+//param info: refer to ToString and ToCsv usage
+//returns 0 if unsuccessful, 1 if successful
+int ToComparisonString(struct Node* node, char* string){
+	if(node == NULL){
+		printf("ToComparisonString(char *string, struct Node* node); -- Node is null, nothing to convert.\n");
+		return 0;
+	}
+	
+	//to string and inverting gpa to maintain descending order
+	char gpaStr[10];
+	ftoa((5.0f-node->GPA), gpaStr, 2);
+
+	//ORDER OF COMPARISON:
+	//inverted gpa, last name, first name, major
+	snprintf(string, 256, "%.2f%s%s%s\n", gpaStr, node->lastName, node->firstName, node->major);
+	return 1;
+}
 
 //not transferable to general linked list functionality?
-void InsertByGPA(struct Node** start, struct Node* nodeToInsert) {	
+//returns 0 if unsuccessful, 1 if successful
+int InsertByGPA(struct Node** start, struct Node* nodeToInsert) {	
+	//validate GPA data first
+	if(nodeToInsert->GPA < 0 || nodeToInsert->GPA > 4.0f){
+		printf("InsertByGPA(struct Node** start, struct Node* nodeToInsert); -- Invalid GPA.\n");
+		return 0;
+	}
+
+
 	if (GetListLength(start) == 0) {
 		InsertFront(start, nodeToInsert);
-		return;
+		return 1;
 	} 
 	//compare with head first
 
 	char nodeToInsertStr[256]; //convert it to a string to compare entir ething alphabetically
-	char toInsertGpaStr[10];
-	ftoa((5.0f-nodeToInsert->GPA), toInsertGpaStr, 2); //'inverse' of gpa goes first so i can sort completely alphabetically and still get largest gpa first
-	strcpy(nodeToInsertStr, toInsertGpaStr); 
-	strcat(nodeToInsertStr, nodeToInsert->lastName);
-	strcat(nodeToInsertStr, nodeToInsert->firstName);
-	strcat(nodeToInsertStr, nodeToInsert->major);
+	ToComparisonString(nodeToInsert, nodeToInsertStr);
 
 	struct Node* current = *start;
 	char currentStr[256]; 
-	char currentGpaStr[10];
-	ftoa((5.0f-current->GPA), currentGpaStr, 2); //'inverse' of gpa goes first so i can sort completely alphabetically and still get largest gpa first
-	strcpy(currentStr, currentGpaStr); 
-	strcat(currentStr, current->lastName); //insert the rest of them to compare alphabetically in order
-	strcat(currentStr, current->firstName);
-	strcat(currentStr, current->major);
+	ToComparisonString(current, currentStr);
 
 	int comp = strcmp(nodeToInsertStr, currentStr);
 	if(comp < 0) {//smaller alphabetically
 		InsertFront(start, nodeToInsert);
-		return;
+		return 1;
 	}
 
 	current = (*start)->next; //start iterating from second item to utilize InsertMiddle()
 	int count = 0; //counter, index to use InsertMiddle() on
 	while (current != NULL) {
 		currentStr[0] = '\0'; //clear strings
-		currentGpaStr[0] = '\0';
-		ftoa((5.0f-current->GPA), currentGpaStr, 2); //'inverse' of gpa goes first so i can sort completely alphabetically and still get largest gpa first
-		strcpy(currentStr, currentGpaStr); 
-		strcat(currentStr, current->lastName); //insert the rest of them to compare alphabetically in order
-		strcat(currentStr, current->firstName);
-		strcat(currentStr, current->major);
+		ToComparisonString(current, currentStr);
+
 		comp = strcmp(nodeToInsertStr, currentStr);
-		
 		if(comp < 0) { //alphabetically smaller, inserting before current node
 			InsertMiddle(start, count, nodeToInsert);
-			return;
+			return 1;
 		}
 		current = current->next;
 		count++;
 	}
 	//reached the end of list and it's not bigger than anything, so inserting at end
 	InsertEnd(start, nodeToInsert);
+	return 1;
 }
 
 // EXAMPLE: void DeleteFront() { WHERE _is global
@@ -359,20 +398,25 @@ void InsertByGPA(struct Node** start, struct Node* nodeToInsert) {
 //	_head = _head->next; //make head point to 2nd item in list
 //	free(temp); //free temp pointer
 
-void DeleteFront(struct Node** start) {
+
+//returns 0 if unsuccessful, 1 if successful
+int DeleteFront(struct Node** start) {
 	if (GetListLength(start) == 0) {
 		printf("DeleteFront(struct Node** start); -- Empty list, nothing to delete\n");
-		return;
+		return 0;
 	}
 	struct Node* temp = *start; //temp pointer to hold current head
 	*start = (*start)->next; //make head point to 2nd item in list
 	free(temp); //free temp pointer
+
+	return 1;
 }
 
-void DeleteEnd(struct Node** start){
+//returns 0 if unsuccessful, 1 if successful
+int DeleteEnd(struct Node** start){
 	if (GetListLength(start) == 0) {
 		printf("DeleteEnd(struct Node** start); -- Empty list, nothing to delete\n");
-		return;
+		return 0;
 	}
 	if ((*start)->next == NULL) {
 		FreeList(start);
@@ -383,34 +427,38 @@ void DeleteEnd(struct Node** start){
 	}
 	free(current->next);
 	current->next = NULL;
+	return 1;
 }
 
-void DeleteMiddle(struct Node** start, int index){
+//returns 0 if unsuccessful, 1 if successful
+int DeleteMiddle(struct Node** start, int index){
 	if (GetListLength(start) == 0) {
 		printf("DeleteMiddle(struct Node** start, int index); -- Empty list, nothing to delete.\n");
-		return;
+		return 0;
 	}
 	struct Node* nodeToDelete = LookUpByIndex(start, index);
 	if(nodeToDelete == NULL) {
 		printf("DeleteMiddle(struct Node** start, int index); -- No node exists at given index to delete.\n");
-		return;
+		return 0;
 	}
-	struct Node* nodeBefore = LookUpByIndex(start, index-1);
-	if(nodeBefore == NULL) { //means the index given was 0, so delete front node
+
+
+
+	if(index == 0) { //means the index given was 0, so delete front node
 		DeleteFront(start);
-		printf("DeleteMiddle(struct Node** start, int index); -- Requested node to delete is start of list, deleting head node.\n");
-
-		return;
+		return 1;
 	}
-	struct Node* nodeAfter = LookUpByIndex(start, index+1);
-	if (nodeAfter == NULL) { //means that index given was last in list, so delete end
+
+	if (index == GetListLength(start)) { //means that index given was last in list, so delete end
 		DeleteEnd(start);
-		printf("DeleteMiddle(struct Node** start, int index); -- Requested node to delete is end of list, deleting end node.\n");
-		return;
+		return 1;
 	}
 
+	struct Node* nodeBefore = LookUpByIndex(start, index-1);
+	struct Node* nodeAfter = LookUpByIndex(start, index+1);
 	nodeBefore->next = nodeAfter;
 	free(nodeToDelete);
+	return 1;
 }
 
 int main() {
@@ -438,14 +486,14 @@ int main() {
 
 		switch(functionNumber){
 			case 1:
-				printf("Here is the list of all your students: ----------------------------------------\n");
+				printf("\nHere is the list of all your students: ----------------------------------------\n");
 				PrintList(&_head);
 				printf("-------------------------------------------------------------------------------\n");
 				break;
 
 			case 2:
 				//insert a student to linked list
-				printf("Enter the information of the student you would like to add.\n");
+				printf("\nEnter the information of the student you would like to add.\n");
 
 				char newStudentFirstName[64], newStudentLastName[64], newStudentMajor[64];
 				float newStudentGPA;
@@ -470,17 +518,19 @@ int main() {
 				scanf("%s", confirmation);
 				
 				if(strcmp(confirmation, "y") == 0){
-					InsertByGPA(&_head, currentStudent);
-					printf("New node added!\n");
-				} else {
-					printf("Add node action cancelled.\n");
-					studentKeyCounter--;//free usage of ID
+					int isSuccessful = InsertByGPA(&_head, currentStudent);
+					if(isSuccessful){
+						printf("New node added!\n");
+						break;
+					}
 				}
+				printf("Add node action cancelled.\n");
+				studentKeyCounter--;//free usage of ID
 				break;
 
 			case 3:
 				//delete a student from linked list by key
-				printf("Enter the key of the student you would like to delete.\n");
+				printf("\nEnter the key of the student you would like to delete.\n");
 
 				int delStudentKey;
 				char delStudentKeyStr[64];
@@ -505,17 +555,19 @@ int main() {
 				printf("Enter y to confirm this is correct, any other key to deny: ");
 				scanf("%s", confirmation);
 				if(strcmp(confirmation, "y") == 0){
-					//do action
-					DeleteMiddle(&_head, GetIndexByKey(&_head, delStudentKey));
-					printf("Node deleted!\n");
-					break;
+					//do action, check for success
+					int isSuccessful = DeleteMiddle(&_head, GetIndexByKey(&_head, delStudentKey));
+					if (isSuccessful) {
+						printf("Node deleted!\n");
+						break;
+					}
 				} 
 				printf("Delete node by key action cancelled.\n");
 				break;
 
 			case 4:
 				//delete student from ll by index
-				printf("Enter the index of the student you would like to delete.\n");
+				printf("\nEnter the index of the student you would like to delete.\n");
 				printf("Reminder: indices start from 0.\n");
 
 				int delStudentIndex;
@@ -541,10 +593,12 @@ int main() {
 				printf("Enter y to confirm this is correct, any other key to deny: ");
 				scanf("%s", confirmation);
 				if(strcmp(confirmation, "y") == 0){
-					//do action
-					DeleteMiddle(&_head, delStudentIndex);
-					printf("Node deleted!\n");
-					break;
+					//do action, check for successs
+					int isSuccessful = DeleteMiddle(&_head, delStudentIndex);
+					if (isSuccessful){
+						printf("Node deleted!\n");
+						break;
+					}
 				} 
 				printf("Delete node by index action cancelled.\n");
 				break;
@@ -554,7 +608,7 @@ int main() {
 				currentStudent = LookUpByIndex(&_head, 0);
 
 				char currentStudentString[256];
-				ToCsv(currentStudentString, currentStudent);
+				ToCsv(currentStudent, currentStudentString);
 				printf("%s", currentStudentString);
 				break;
 			case 6:
@@ -562,12 +616,11 @@ int main() {
 				FreeList(&_head);
 				break;
 			default:
-				printf("\nSorry, that is an invalid input.\n");
+				printf("\nSorry, that is an invalid operation.\n");
 				userInputCounter--;
 		}
 		
 	} while (cont == 1);
 
-	return 0;
-
+	return 1;
 }
